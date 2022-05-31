056e8840c3e95e19f0277599b5d1ef9aed743c68
Devansh Verma <35062592+Orion6400@users.noreply.github.com>
2022-05-31 22:25:06 +0530

    Update views.py

--- climate/weatherproject/weatherapp/views.py
+++ climate/weatherproject/weatherapp/views.py
@@ -9,17 +9,17 @@ def homepage(request):
         city = request.POST['city']
         source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=7bc29e275d67c72561195573b30ad78c').read()
         dict_data = json.loads(source)
-        cur_time = datetime.now()
+        cur_time = datetime.now().replace(tzinfo=None).strftime("%m/%d/%Y, %H:%M:%S")
 
         weather_data.objects.create(country_name=(str(city)).capitalize() ,country_code=str(dict_data["sys"]["country"]),
                                     coordinates=str(dict_data["coord"]["lon"])+"|"+str(dict_data["coord"]["lat"]),temp=str(dict_data["main"]["temp"]),
                                     real_temp_feel=str(dict_data["main"]["feels_like"]),pressure=str(dict_data["main"]["pressure"]),humidity=str(dict_data["main"]["humidity"]),
-                                    wind_speed=str(dict_data["wind"]["speed"]),description=(str(dict_data["weather"][0]["description"])).capitalize())
+                                    wind_speed=str(dict_data["wind"]["speed"]),description=(str(dict_data["weather"][0]["description"])).capitalize(),record_entry_date=str(cur_time))
 
         data = {
             "country_name": str(city),"country_code": str(dict_data["sys"]["country"]),"coordinates": str(dict_data["coord"]["lon"])+"|"+str(dict_data["coord"]["lat"]),
             "temp": str(dict_data["main"]["temp"]),"real_temp_feel": str(dict_data["main"]["feels_like"]),"pressure": str(dict_data["main"]["pressure"]),
-            "humidity": str(dict_data["main"]["humidity"]),"wind_speed":str(dict_data["wind"]["speed"]),"description":str(dict_data["weather"][0]["description"])
+            "humidity": str(dict_data["main"]["humidity"]),"wind_speed":str(dict_data["wind"]["speed"]),"description":str(dict_data["weather"][0]["description"]),"Time":cur_time
         }
     else:
         data = {}
@@ -29,9 +29,12 @@ def homepage(request):
 def recordpage(request):
     recorded_data = weather_data.objects.filter().values()
     if request.method == 'POST':
-        id = request.POST.get('sno', False)
-        print(id)
-        weather_data.objects.filter(id=id).delete()
+        id = request.POST.get('sno')
+        try:
+            print(id)
+            weather_data.objects.filter(id=id).delete()
+        except Exception as e:
+            print(e)
         return redirect('http://127.0.0.1:8000/record/')
     return render(request,"main/record.html",{"record":recorded_data})
 
